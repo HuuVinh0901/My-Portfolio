5bbee1bed200166145f1c95919d159f63280eed9
Vinh <phamhuuvinh0901@gmail.com>
2024-08-02 19:52:22 +0700

    submit contact

--- components/contact.js
+++ components/contact.js
@@ -1,18 +1,12 @@
-document.getElementById("myForm").addEventListener("submit", function(event){
+document.getElementById("myForm").addEventListener("submit", function (event) {
     event.preventDefault();
-    // Hiển thị modal
-    var modal = document.getElementById("successModal");
-    modal.style.display = "block";
-    
-    // Ẩn modal khi bấm nút đóng
-    document.querySelector(".close-button").onclick = function() {
-        modal.style.display = "none";
-    };
-    
-    // Ẩn modal khi bấm ra ngoài modal
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
+    Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Thanks for leaving me a message!",
+        showConfirmButton: false,
+        timer: 3000
+      });
+    document.getElementById("emailInput").value = "";
+    document.getElementById("textArea").value = "";
 });
